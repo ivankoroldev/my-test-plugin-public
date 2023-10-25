68c3ba760f5aa923abf572c29540faa1b0c43163
ivankoroldev <dev.ivankorol@gmail.com>
2023-10-25 16:25:30 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const Utilities = Utilities || (function () {
+
+    //Cannot be called from outside this function
+    const privateFunction = () => {
+        const box = document.getElementById(Utilities.settings.wrapperId);
+        console.log(box);
+    }
+
+    //Return only what must be publicly accessible, in this
+    //case only the show() method
+    return {
+        init: () => {
+            privateFunction();
+        }
+    }
+})();
+
+Utilities.settings = {
+    wrapperId: 'some'
+}
