98f985b995f6cc6e7dc28ee585c0eb433a34c6ff
ivankoroldev <dev.ivankorol@gmail.com>
2023-10-25 19:34:26 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,27 +1,24 @@
-let Utilities;
+var Utilities = Utilities || (function () {
 
-window.onload = function(){
-    Utilities = Utilities || (function () {
-
-        //Cannot be called from outside this function
-        const privateFunction = () => {
-            const box = document.getElementById(Utilities.settings.wrapperId);
-            console.log(Utilities.settings)
-            console.log(box);
-        }
-
-        //Return only what must be publicly accessible, in this
-        //case only the show() method
-        return {
-            init: (userSettings) => {
-                Utilities.settings = {...Utilities.settings, ...userSettings};
-                privateFunction();
-            }
-        }
-    })();
-
-    Utilities.settings = {
-        wrapperId: 'some',
+    //Cannot be called from outside this function
+    const privateFunction = () => {
+        const box = document.getElementById(Utilities.settings.wrapperId);
+        console.log(Utilities.settings)
+        console.log(box);
+        box.innerHTML =
+            '<object width="100%" type="text/html" data="./index.html"</object>';
     }
-};
 
+    //Return only what must be publicly accessible, in this
+    //case only the show() method
+    return {
+        init: (userSettings) => {
+            Utilities.settings = {...Utilities.settings, ...userSettings};
+            privateFunction();
+        }
+    }
+})();
+
+Utilities.settings = {
+    wrapperId: 'some',
+}
